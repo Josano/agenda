17a80285ad34cdf356c535db856fa7f303e32b15
josanosouza <josano.soares@scnbrasil.com.br>
2019-09-03 17:38:08 -0300

    Iniciando listagem

--- core_agenda/models.py
+++ core_agenda/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 
@@ -14,6 +15,8 @@ class Evento(models.Model):
     class Meta:
         db_table = 'evento'
 
-
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
